40f343f5ca73364ffa214363be604724854dae70
mess <mess.ren@itering.io>
2022-03-22 14:25:31 +0800

    feat(hooks): add useFetch and useFetchEventList hook

---
+++ src/config/db.ts
@@ -0,0 +1,6 @@
+export enum LocalKeys {
+  AIRDROP_PROJECTS = 'AIRDROP_PROJECTS'
+}
+
+// TODO: preparation for indexDB、webSQL and sessionStorage
+export const dummy = null
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,50 @@
+import { useCallback, useState } from 'react'
+
+interface IFetchErrorConstructor {
+  code: number
+  message: string
+}
+
+class FetchError extends Error {
+  code: number
+
+  constructor({ code, message }: IFetchErrorConstructor) {
+    super()
+    this.code = code
+    this.message = message
+  }
+}
+
+const useFetch = () => {
+  const [isLoading, setStatus] = useState(true)
+
+  const fetchData = useCallback(
+    async <T>(url: string): Promise<T | undefined> => {
+      setStatus(true)
+      let res
+      try {
+        const response = await fetch(url)
+        const { ok, status, statusText } = response
+        if (!ok) {
+          throw new FetchError({
+            code: status,
+            message: statusText
+          })
+        }
+        res = await response.json()
+      } catch (e) {
+        if (e instanceof Error) {
+          // throw
+        }
+      }
+
+      setStatus(false)
+      return res
+    },
+    []
+  )
+
+  return { isLoading, fetchData }
+}
+
+export default useFetch
