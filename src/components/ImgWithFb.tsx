555da0c9cb56370893fc8349a68d11f9dfb4b7c4
mess <mess.ren@itering.io>
2022-04-12 16:39:56 +0800

    refactor: replace Img with chakra Img

--- src/components/ImgWithFb.tsx
+++ src/components/ImgWithFb.tsx
@@ -1,6 +1,6 @@
 import { Image, ImageProps } from '@chakra-ui/react'
 import React from 'react'
-import imgFallback from './img-fallback.png'
+import imgFallback from '@/assets/img-fallback.png'
 
 const ImgWithFb = (props: ImageProps) => (
   <Image fallbackSrc={imgFallback} {...props} />
