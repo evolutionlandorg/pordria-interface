b7c0e059b4756d3e8f02709939941c791aadff71
mess <mess.ren@itering.io>
2022-04-12 16:41:32 +0800

    refactor: update Loading component image

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,56 +1,12 @@
-import { computeSize, size, weight } from '@/styles/variables'
+import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
-import { LoadingIcon } from './Icon'
-
-const LoadingWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding-top: ${computeSize(122)};
-  font-size: ${size.sm};
-  font-weight: ${weight.semiBold};
-`
-const LoadingRocket = styled(LoadingIcon)`
-  width: ${computeSize(30)};
-`
-
-const dot = keyframes`
-  0% {
-    transform: translateX(0.5ch);
-  }
-
-  50% {
-    transform: translateX(1ch);
-  }
-
-  100% {
-    transform: translateX(1.5ch);
-  }
-`
-
-const Dot = styled.span`
-  position: relative;
-
-  ::before {
-    content: '';
-    background-color: white;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    animation: ${dot} 2s steps(3, end) infinite;
-  }
-`
+import loading from '@/assets/loading.png'
 
 function Loading() {
   return (
-    <LoadingWrap>
-      <LoadingRocket />
-      <p>
-        Loading<Dot>...</Dot>
-      </p>
-    </LoadingWrap>
+    <Flex direction="column" align="center" justify="center" pt="25">
+      <Image src={loading} boxSize="16" />
+    </Flex>
   )
 }
 
